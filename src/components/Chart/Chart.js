3481ee9c437f72b3c18094616bc8ca654dce5064
Pallab Hk <horakrishnaroy@gmail.com>
2020-04-16 21:37:12 +0600

    This is api landing page

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,48 @@
+import React,{ useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
+const Chart =() => {
+
+  const[ dailyData, setDailyData ] = useState({});
+   
+ useEffect( () =>{
+    const fetchAPI = async () =>{
+      setDailyData(await fetchDailyData());
+    }
+    
+
+    fetchAPI();
+ });
+
+ const lineChart = (
+  dailyData[0]
+  ? (
+    <Line
+      data ={{
+        labels: dailyData(({date}) => date),
+        database:[{
+          data :dailyData(({confirmed}) => confirmed),
+          label:'infected',
+          borderColor:'#3333ff',
+          fill :true,
+
+        } ,{
+          data :dailyData(({ deaths }) => deaths),
+          label:'Deaths',
+          borderColor:'red',
+          backgroundColor: 'rgba(255, 0, 0,.5)',
+          fill :true,
+        }]
+      }}
+    />) :null
+ );
+  return(
+         <div className={ styles.container }>
+           { lineChart }
+         </div>  
+        )
+    
+}
+export default Chart;
